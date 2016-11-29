9567497386c28a733fdf789d4101da7a8bb5abd7
Flowless <daniel.hedemalm@gmail.com>
2016-11-29 22:41:37 +0000

    Fernet-encryption has been added

--- pycryptium.py
+++ pycryptium.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 
 #from file.py import *
+from cryptography.fernet import Fernet
 import sys
 
 def encrypt():
-	print("You have chosen Encryption. AES will be used.")
+	print("You have chosen Encryption. AES_128_CBC_PKCS7 is used.")
 	inp = None
 	while not inp: 
 		inp = raw_input('File for encryption: ')
 	output = raw_input('Output: ')
 	if not output:
 		print("STDOUT is used for output.")
+	#Key-generate, Base64-encoded, SECRET, SYMMETRIC ENCRYPTION
+	for i in range(0,9):
+		key = Fernet.generate_key()
+		print(key)
+	#Creates a Fernet-object with the given key
+	f = Fernet(key)
+	#Creates the token (the enc content)
+	token = f.encrypt(b"this is super secret")
 
 def decrypt():
-	print("You have chosen Decryption. AES will be decrypted.")
+	print("You have chosen Decryption. AES_128_CBC_PKCS7 will be decrypted.")
 	inp = None
 	while not inp: 
 		inp = raw_input('File for encryption: ')
